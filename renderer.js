687d1d6d835493a3b69977747fad5b7fbae2b1e4
gentilmente <pestequiel@gmail.com>
2021-11-13 21:16:47 -0300

    first

---
+++ renderer.js
@@ -0,0 +1,69 @@
+const camera = require("./scripts/camera")
+const livePreview = require("./scripts/live-preview")
+
+const { getCurrentWindow } = require("electron");
+
+let livePrev;
+
+camera.initialize(function (res, msg, err) {
+
+  if (!res) {
+    console.error("camera:", msg, err);
+    return
+  }
+
+  livePrev = new livePreview(
+    camera.camera,
+    document.getElementById("live"),
+    10
+  );
+  livePrev.start();
+
+});
+
+/*
+ * Trigger photo when clicking / touching anywhere at the screen
+ */
+document.body.addEventListener("click", () => trigger())
+
+
+function trigger(callback) {
+  if (callback === undefined) {
+    callback = function () { };
+  }
+
+  if (executing) {
+    callback(true);
+    return;
+  }
+
+  executing = true;
+
+  if (camera.isInitialized()) {
+    // take picture after countdown
+    setTimeout(function () {
+      if (livePrev) livePrev.stop();
+
+      camera.takePicture(function (res, msg1, msg2) {
+        const message1 = msg1;
+        const message2 = msg2;
+      });
+    }, (countdownLength) * 1000);
+  } else {
+    // TODO: Handle uninitialized camera
+
+    camera.initialize(function (res, msg, err) {
+      if (res) {
+        executing = false;
+        trigger(callback);
+      } else {
+        callback(false);
+      }
+    });
+  }
+}
+
+/*
+ * Module exports
+ */
+module.exports.triggerPhoto = trigger;
---
+++ scripts/camera.js
@@ -0,0 +1,113 @@
+const gphoto2 = require("gphoto2");
+
+class Camera {
+  constructor() {}
+
+  /*
+   * Detect and configure camera
+   */
+  initialize(callback) {
+    if (gphoto2 === null) {
+      callback(true);
+      return;
+    }
+
+    this.GPhoto = new gphoto2.GPhoto2();
+
+    // Negative value or undefined will disable logging, levels 0-4 enable it.
+    this.GPhoto.setLogLevel(-1);
+
+    var self = this;
+    this.GPhoto.list(function (list) {
+      if (list.length === 0) {
+        callback(false, "No camera found", null);
+        return;
+      }
+      self.camera = list[0];
+
+      console.log("gphoto2: Found", self.camera.model);
+
+      self.camera.setConfigValue("capturetarget", 1, function (err) {
+        if (err) {
+          callback(false, "setting config failed", err);
+        } else {
+          callback(true);
+        }
+      });
+    });
+  }
+
+  isInitialized() {
+    return this.camera !== undefined || gphoto2 === null;
+  }
+
+  isConnected(callback) {
+    if (gphoto2 === null) {
+      callback(true);
+      return;
+    }
+
+    this.camera.getConfig(function (err, settings) {
+      if (err) {
+        if (callback) callback(false, "connection test failed", err);
+      } else {
+        self.camera == undefined; // needs to be reinitialized
+        if (callback) callback(true);
+      }
+    });
+  }
+
+  takePicture(callback) {
+    if (gphoto2 !== null) {
+      this._takePictureWithCamera(callback);
+    } else {
+      this._createSamplePicture(callback);
+    }
+  }
+
+  _takePictureWithCamera(callback) {
+    var self = this;
+
+    if (self.camera === undefined) {
+      callback(-1, "camera not initialized", null);
+      return;
+    }
+
+    const keep = true;
+
+    self.camera.takePicture(
+      { download: true, keep: keep },
+      function (err, data) {
+        if (err) {
+          self.camera = undefined; // needs to be reinitialized
+          callback(-2, "connection to camera failed", err);
+          return;
+        }
+
+        self._resizeAndSave(data, callback);
+      }
+    );
+  }
+
+  _createSamplePicture(callback) {
+    console.log("sample picture");
+  }
+
+  _resizeAndSave(data, callback) {
+    const filename = "img_" + "foterli" + ".jpg";
+
+    function resizeInternal() {
+      const resizedFilePath = __dirname + filename;
+      const webFilepath = "photos/" + "img_" + "foterlic" + ".jpg";
+      const maxImageSize = 1500;
+    }
+    resizeInternal();
+  }
+}
+
+/*
+ * Module exports for connection
+ */
+let camera = new Camera();
+//export { camera as default };
+module.exports = camera;
