f0a35c4a50a5fc78d711d5d4794f5e65782b3d29
gentilmente <egentilemontes@gmail.com>
2021-12-20 13:00:46 -0300

    add bulma js notification

--- preload.js
+++ preload.js
@@ -1,12 +1,29 @@
+const BulmaNotification = require("./src/scripts/bulma-notifications");
+
+let notif;
+let notif1;
+
+window.onload = () => {
+  notif = new BulmaNotification();
+  document
+    .querySelector("#toggle-modal")
+    .addEventListener("click", showNotification);
+};
+
+// Display a notification
+function showNotification() {
+  notif.show("Notification Title", "Notification message", "primary", 2000);
+}
+
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, process.versions[type]);
   }
-})
+});
