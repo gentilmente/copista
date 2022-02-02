ffcf79a207216f9c7bf67ca9abf59cdd56801aa3
gentilmente <egentilemontes@gmail.com>
2022-02-01 21:35:56 -0300

    sync send to render, async wait to setting(batery)

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,7 @@
 console.log('renderer...');
-
+const bat = document.getElementById('bat');
 const model = document.getElementById('model');
+
 window.biblioApi.onInitCamera((e, msg) => {
   console.log(
     '🚀 ~ file: renderer.js ~ line 5 ~ window.biblioApi.onInitCamera ~ msg',
@@ -8,4 +9,14 @@ window.biblioApi.onInitCamera((e, msg) => {
   );
   model.innerHTML = msg;
   window.biblioApi.notification('oh no!', msg, 'danger');
+});
+
+window.biblioApi.onInitCam((e, level) => {
+  console.log(
+    '🚀 ~ file: renderer.js ~ line 14 ~ window.biblioApi.onInitCam ~ msg',
+    level
+  );
+
+  bat.innerHTML = level.value;
+  window.biblioApi.notification('yes!', level.value, 'success');
 });
